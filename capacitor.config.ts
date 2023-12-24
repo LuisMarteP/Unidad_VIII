967e6406456d6af90dc4f43dc2ec41794e801a8b
LuisMarteP <luisandrymartes@gmail.com>
2023-12-24 09:15:55 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MVClips_v2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
